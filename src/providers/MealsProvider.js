43273235715b19246ea6c3db74d36a06984b61e9
cfigueiroa <cfigueiroa@gmail.com>
2022-11-16 10:33:16 -0300

    hello world

---
+++ src/providers/MealsProvider.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MealsContext = React.createContext();
+
+const todaysMeals = ["Breakfast", "Lunch", "Dinner"];
+
+const MealsProvider = ({ children }) => {
+
+    const [meals, setMeals] = React.useState(todaysMeals);
+
+    return (
+        <MealsContext.Provider value={{ meals }} >
+            {children}
+        </MealsContext.Provider>
+    );
+}
+
+export const useMealsListContext = () => React.useContext(MealsContext);
+
+export default MealsProvider;
